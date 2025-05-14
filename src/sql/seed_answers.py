a3fe65214ca42db48e273642402c1d5b4d67e0e5
Luca Rijneveld <76219488+LucaRijneveld@users.noreply.github.com>
2025-05-14 22:03:48 +0200

    other added to db

--- src/sql/seed_answers.py
+++ src/sql/seed_answers.py
@@ -7,38 +7,11 @@ engine = create_engine("sqlite:///src/sql/portfolio.db", echo=True)
 Base.metadata.create_all(engine)
 
 # Answers to populate
-q1_answers = ["LinkedIn", "GitHub", "Google", "University", "Friend", "Other"]
-q2_answers = ["Tech", "Gaming", "Academia", "Business", "Other"]
-q3_answers = ["Data Science", "AI", "AI & Games", "Business Analytics"]
-q4_answers = [
-    "Recruiter",
-    "Student",
-    "Lecturer",
-    "Researcher",
-    "Developer",
-    "Founder",
-    "Other",
-]
 q5_answers = [
-    "Minecraft",
-    "League of Legends",
-    "World of Warcraft",
-    "Valorant",
-    "Elden Ring",
-    "Dungeons & Dragons",
-    "Apex Legends",
-    "None",
+    "Other"
 ]
 
 with Session(engine) as session:
-    for ans in q1_answers:
-        session.add(AnswersQ1(answer=ans))
-    for ans in q2_answers:
-        session.add(AnswersQ2(answer=ans))
-    for ans in q3_answers:
-        session.add(AnswersQ3(answer=ans))
-    for ans in q4_answers:
-        session.add(AnswersQ4(answer=ans))
     for ans in q5_answers:
         session.add(AnswersQ5(answer=ans))
 
