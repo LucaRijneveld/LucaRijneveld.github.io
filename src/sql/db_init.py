aa7bcf7ab301c0d00b001a6a7d626970993bd0d7
Lokbee <76219488+LucaRijneveld@users.noreply.github.com>
2025-05-14 09:25:35 +0000

    db init change

--- src/sql/db_init.py
+++ src/sql/db_init.py
@@ -1,10 +1,8 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 
+engine = create_engine("postgresql://localhost/mydb")
+if not database_exists(engine.url):
+    create_database(engine.url)
 
-# PostgreSQL database URL
-database_url = 'postgresql://username:password@host/database_name'
-
-engine = create_engine("postgresql://user:password@localhost:5432/portfolio")
-Base.metadata.create_all(engine)
+print(database_exists(engine.url))
